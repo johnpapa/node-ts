8adaa0e243f9093baf2729693e07225f8c2ff915
John Papa <john@johnpapa.net>
2018-08-10 15:22:22 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as http from 'http';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
